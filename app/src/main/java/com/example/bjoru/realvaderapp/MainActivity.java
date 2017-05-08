37ac3ba6192cd5df67fe510b6769a54d4c099bdb
Gislebjoru <142041@student.nord.no>
2017-05-08 15:35:35 +0200

    test commit3

--- app/src/main/java/com/example/bjoru/realvaderapp/MainActivity.java
+++ app/src/main/java/com/example/bjoru/realvaderapp/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
             public void processFinish(WeatherData output) {
 
 
-                //github dl og push fra laptop
+                //github dl og push fra laptop ayy lmao
                 Temperature temp = output.getForecast().getTimeList().get(0).getTemperature();
                 WindSpeed wspeed = output.getForecast().getTimeList().get(0).getWindSpeed();
                 Pressure pressure = output.getForecast().getTimeList().get(0).getPressure();
