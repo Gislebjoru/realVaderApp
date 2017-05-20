9cc296afc4c7b2c16c01b0422465d2c5ccaa5ace
gislebjoru <gisle_fb@yahoo.no>
2017-05-20 21:05:36 +0200

    asdnmkaklmsdklasmdkad

--- app/src/main/java/com/example/bjoru/realvaderapp/MainActivity.java
+++ app/src/main/java/com/example/bjoru/realvaderapp/MainActivity.java
@@ -2,8 +2,9 @@ package com.example.bjoru.realvaderapp;
 
 import android.content.Intent;
 import android.location.Location;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
