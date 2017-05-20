4df21901919da8d5b78c3ea69271775b5ebde5bc
gislebjoru <gisle_fb@yahoo.no>
2017-05-20 21:47:26 +0200

    Latitude longitude

--- app/src/main/java/com/example/bjoru/realvaderapp/MainActivity.java
+++ app/src/main/java/com/example/bjoru/realvaderapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.bjoru.realvaderapp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -21,10 +26,6 @@ import com.google.android.gms.location.LocationServices;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements ConnectionCallbacks, OnConnectionFailedListener {
-
-    //public static final String myString = "myString";
-    //public static String city = "";
-
     protected static final String TAG = "MainActivity";
 
     protected GoogleApiClient mGoogleApiClient;
@@ -37,14 +38,15 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
     protected TextView mLatitudeText;
     protected TextView mLongitudeText;
 
+    final int LOCATION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //mLatitudeLabel = getResources().getString(R.string.latitude_label);
-        //mLongitudeLabel = getResources().getString(R.string.longitude_label);
+        mLatitudeLabel = getResources().getString(R.string.latitude_label);
+        mLongitudeLabel = getResources().getString(R.string.longitude_label);
         mLatitudeText = (TextView) findViewById(R.id.latitude_text);
         mLongitudeText = (TextView) findViewById(R.id.longitude_text);
 
@@ -52,21 +54,24 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
 
         final Intent intent = new Intent(this, Langtidsvarsel.class);
 
-
-
         final TextView tempText = (TextView)findViewById(R.id.tempText);
         final TextView wsText = (TextView)findViewById(R.id.wsText);
         final TextView wdText = (TextView)findViewById(R.id.wdText);
         final TextView pressureText = (TextView)findViewById(R.id.presText);
         //final ImageView symbolImg = (ImageView)findViewById(R.id.myImg);
-       // final TextView testFelt = (TextView) findViewById(R.id.textView2);
+        //final TextView testFelt = (TextView) findViewById(R.id.textView2);
         final EditText sokeFelt = (EditText) findViewById(R.id.sokeFelt);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+        } else {
+            System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        }
 
         new VaderData(new VaderData.AsyncResponse() {
             @Override
             public void processFinish(WeatherData output) {
-
-                //String test = String.valueOf(VaderData.newUrl[5]+" "+VaderData.newUrl[6]+" "+VaderData.newUrl[7]);
                 List<Time> timeList = output.getForecast().getTimeList();
                 final String timeFrom = output.getForecast().getTimeList().get(0).getFrom();
                 final String timeTo = output.getForecast().getTimeList().get(0).getTo();
@@ -76,11 +81,6 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
                 WindDirection wdir = output.getForecast().getTimeList().get(0).getWindDirection();
                 Symbol symbol = output.getForecast().getTimeList().get(0).getSymbol();
 
-                System.out.println(String.valueOf("Temperatur: "+temp.getValue())+" "+temp.getUnit());
-                System.out.println(String.valueOf("Vindstyrke: "+wspeed.getMps())+" Meter i sekundet, "+wspeed.getName());
-                System.out.println(String.valueOf("Lufttrykk: "+pressure.getValue()+" "+pressure.getUnit()));
-                System.out.println(String.valueOf("Vindretning: "+wdir.getDeg()+" "+wdir.getCode()+" "+wdir.getName()));
-                System.out.println(symbol);
                 tempText.setText(String.valueOf(temp.getValue()+" "+temp.getUnit()));
                 wsText.setText(String.valueOf(wspeed.getMps()+" Meter i sekundet, "+wspeed.getName()));
                 wdText.setText(String.valueOf(wdir.getName()+" Vindretning"));
@@ -89,26 +89,15 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
                 } else {
                     pressureText.setText(String.valueOf("Lavtrykk"+"("+pressure.getValue()+" hPa)"));
                 }
-                //System.out.println(test);
-
-
-                //symbolImg.setImageResource(R.drawable.class);
 
                 final Button langtidsvarsel = (Button) findViewById(R.id.button2);
                 langtidsvarsel.setOnClickListener(new View.OnClickListener() {
                     public void onClick(View view) {
-                        //String message = String.valueOf(timeFrom+"-"+timeTo);
-                        //intent.putExtra(myString, t);
                         startActivity(intent);
                     }
                 });
-
-
-
             }
         }).execute();
-
-
 
         final Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -126,13 +115,10 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build();
-
-
     }
 
     @Override
     public void onConnected(Bundle connectionHint) {
-
         mLastlocation= LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         if (mLastlocation != null) {
             mLatitudeText.setText(String.format("%s: %f", mLatitudeLabel,
@@ -166,5 +152,17 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
     @Override
     public void onConnectionFailed(ConnectionResult result) {
         Log.i(TAG, "Connection failed: connectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode) {
+            case LOCATION_REQUEST_CODE:
+                if(grantResults.length > 0) {
+
+                } else {
+
+                }
+        }
     }
 }
